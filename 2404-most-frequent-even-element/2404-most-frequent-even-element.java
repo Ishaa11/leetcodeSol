1e4fe23540c6a018881d109f813768cc9d9c1dd7
Ishaa11 <64044485+Ishaa11@users.noreply.github.com>
2022-11-08 01:19:41 +0530

    Time: 35 ms (76.14%), Space: 62.2 MB (34.64%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,35 +1,30 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
         
-        HashMap<Integer,Integer> map = new HashMap<>();
+    
+ Map<Integer,Integer> map=new HashMap<>();
         
-        for(int i : nums){
-            if(i%2==0){
-                if(map.containsKey(i)){
-                    map.put(i,map.get(i)+1);
-                }else{
-                    map.put(i,1);
-                }
+        int max=-1;
+        int res=Integer.MAX_VALUE;
+        
+        for(int i:nums){
+            
+           
+            if(i%2 == 0){                        //Only even element
+            map.put(i,map.getOrDefault(i,0)+1);
+            
+            
+            if(map.get(i)>max){                 //Check if greater than Max Val
+            max=Math.max(max,map.get(i));
+            res=i;
+            }                                   
+            else if(map.get(i)==max && res>i){  //Check if equals to Max Val and element is less than current res
+            res=i;
+            }
             }
         }
         
-        if(map.isEmpty()) return -1;
-        
-       int freq=0,val=0;
-        for (Map.Entry<Integer,Integer> m : map.entrySet()) {
-            if(freq==0){
-                val=m.getKey();
-                freq=m.getValue();
-            }
-            else if(freq<m.getValue()){
-                val=m.getKey();
-                freq=m.getValue();
-            }
-            else if(freq==m.getValue()){
-                val=(val>m.getKey())?m.getKey():val;
-            }
-        }
-        
-        return val;
+        return res==Integer.MAX_VALUE? -1: res; 
     }
 }
+        
