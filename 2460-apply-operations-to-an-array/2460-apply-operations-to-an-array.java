0c6c5f9b7ec46c94fc113c828003064077c2ac31
Ishaa11 <64044485+Ishaa11@users.noreply.github.com>
2022-11-08 23:46:39 +0530

    Time: 1 ms (89.50%), Space: 44.8 MB (10.44%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        moveZeroes(nums);
+        
+        return nums;
+    }
+    
+    void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
+        }
+    }
+    
+}
