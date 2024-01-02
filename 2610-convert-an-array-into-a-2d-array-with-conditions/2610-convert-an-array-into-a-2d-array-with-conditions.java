213ac0e5855dab2c20233aadcdab53b348d47418
Isha Ghyar <ishaghyar11@gmail.com>
2024-01-02 13:32:24 +0530

    Time: 6 ms (16.31%), Space: 44.8 MB (26.06%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -0,0 +1,63 @@
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        int n=nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        
+       
+        int loop = Frequency(nums);
+        
+        for(int i=0;i<loop;i++){
+            ArrayList<Integer> list1 = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+                
+                int key = entry.getKey();
+                int value = entry.getValue();
+                
+                if(value!=0){
+                    map.put(key, map.get(key) - 1);
+                    list1.add(key);
+                }
+            }
+            list.add(list1);
+            
+        }
+        return list;
+    }
+    
+    static int Frequency(int arr[]) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n=arr.length;
+        
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        int maxFreq = 0, maxEle = 0;
+        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int count = entry.getValue();
+            int element = entry.getKey();
+
+            if (count > maxFreq) {
+                maxEle = element;
+                maxFreq = count;
+            }
+        }
+        
+        return maxFreq;
+    }
+}
